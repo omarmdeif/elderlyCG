7abf0d6550f37a3c8b270557c3839fbaf1d3acc7
Omar <93540748+omarmdeif@users.noreply.github.com>
2023-05-25 19:11:11 +0300

    ..

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from eye_gaze import eyeGaze
-from facial_expression import facialExper 
+from facial_expression import facialExper
+from csvcrud import ccrud
 import cv2
 
 def main():
+    ct = 1
     eg = eyeGaze()
     fe = facialExper()
+    egfe = ccrud()
+    field = ["id", "emotion", "gaze"]
+    egfe.createFile("gazeEmo.csv", field)
     vidcap = cv2.VideoCapture(0)
     while vidcap.isOpened():
         ret, frame = vidcap.read()
         pos = eg.detect_Gaze(frame)
         emo = fe.detectExpr(frame)
         print("emotion, position: ", emo + "," + pos)
+        egfe.writeRow("gazeEmo.csv", [str(ct), emo, pos])
     else:
         print("Cannot open camera")
 
